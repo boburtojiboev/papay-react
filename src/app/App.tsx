5a94dde45c39d388cc67cdbc73e6e4f4623e0ec0
boburtojiboev <tbobur1995@gmail.com>
2023-11-10 22:20:44 +0900

    feat: checking react-router-dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,49 @@ import React from 'react';
 import '../css/App.css';
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
 import { RippleBadge } from './MaterialTheme/styled';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Users from './components/users';
+import Dishes from './components/dishes';
 
 function App() {
   return (
-    <Container>
-      
-    </Container>
-  );
-}
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/dishes">Dishes</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/dishes">
+            <Dishes />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+          <Container>
+          <Home />
+          </Container>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+ );
+}
 export default App;
+
+function Home() {
+  return <h2>Home</h2>;
+}
