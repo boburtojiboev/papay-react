3e0cfa7ca9e787e4afeb3ffe9e4d5c60039abc25
boburtojiboev <tbobur1995@gmail.com>
2023-11-14 01:51:23 +0900

    feat: develop logic of footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import '../css/App.css';
-import '../css/navbar.css';
-import { Box, Button, Container, Stack, Typography } from '@mui/material';
+import React, { useState } from "react";
+import "../css/App.css";
+import "../css/navbar.css";
+import "../css/footer.css";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-import { RestaurantPage } from './screens/RestaurantPage';
-import { CommunityPage } from './screens/CommunityPage';
-import { OrdersPage } from './screens/OrdersPage';
-import { MemberPage } from './screens/MemberPage';
-import { HelpPage } from './screens/HelpPage';
-import { LoginPage } from './screens/LoginPage';
-import { HomePage } from './screens/HomePage';
-import { NavbarHome } from './components/header';
-import { NavbarRestaurant } from './components/header/restaurant';
-import { NavbarOthers } from './components/header/others';
-import { stat } from 'fs';
+import { RestaurantPage } from "./screens/RestaurantPage";
+import { CommunityPage } from "./screens/CommunityPage";
+import { OrdersPage } from "./screens/OrdersPage";
+import { MemberPage } from "./screens/MemberPage";
+import { HelpPage } from "./screens/HelpPage";
+import { LoginPage } from "./screens/LoginPage";
+import { HomePage } from "./screens/HomePage";
+import { NavbarHome } from "./components/header";
+import { NavbarRestaurant } from "./components/header/restaurant";
+import { NavbarOthers } from "./components/header/others";
+import { Footer } from "./components/footer";
 
 function App() {
   const [path, setPath] = useState();
@@ -25,62 +25,37 @@ function App() {
       {main_path == "/" ? (
         <NavbarHome setPath={setPath} />
       ) : main_path.includes("/restaurant") ? (
-        <NavbarRestaurant setPath={setPath}/>
-      ) : ( 
-        <NavbarOthers setPath={setPath}/>
+        <NavbarRestaurant setPath={setPath} />
+      ) : (
+        <NavbarOthers setPath={setPath} />
       )}
-      
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to="/restaurant">RestaurantPage</Link>
-            </li>
-            <li>
-              <Link to="/community">CommunityPage</Link>
-            </li>
-            <li>
-              <Link to="/orders">OrdersPage</Link>
-            </li>
-            <li>
-              <Link to="/member-page">MemberPage</Link>
-            </li>
-            <li>
-              <Link to="/help">HelpPage</Link>
-            </li>
-            <li>
-              <Link to="/login">LoginPage</Link>
-            </li>
-            <li>
-              <Link to="/">HomePage</Link>
-            </li>
-          </ul>
-        </nav> */}
 
-        <Switch>
-          <Route path="/restaurant">
-            <RestaurantPage />
-          </Route>
-          <Route path="/community">
-            <CommunityPage />
-          </Route>
-          <Route path="/orders">
-            <OrdersPage />
-          </Route>
-          <Route path="/member-page">
-            <MemberPage />
-          </Route>
-          <Route path="/help">
-            <HelpPage />
-          </Route>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-          <Route path="/">
+      <Switch>
+        <Route path="/restaurant">
+          <RestaurantPage />
+        </Route>
+        <Route path="/community">
+          <CommunityPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <MemberPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/">
           <HomePage />
-          </Route>
-        </Switch>
-      
+        </Route>
+      </Switch>
+
+      <Footer />
     </Router>
- );
+  );
 }
 export default App;
