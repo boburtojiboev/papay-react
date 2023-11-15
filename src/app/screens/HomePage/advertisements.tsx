040c31ac2ece75eb8238076dbee207246cda502b
boburtojiboev <tbobur1995@gmail.com>
2023-11-15 22:24:55 +0900

    feat: develop homepage statistics component

---
+++ src/app/screens/HomePage/advertisements.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+export function Advertisements() {
+    return (
+        <div className="top_restaurant_frame">
+            Advertisements
+        </div>
+    )
+}
---
+++ src/app/screens/HomePage/recommendations.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+export function Recommendations() {
+    return (
+        <div className="top_article_frame">
+            <Container>Recommendations</Container>
+        </div>
+    )
+}
