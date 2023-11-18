18a768add5ce8c73ef3a396b49278a1509abb366
boburtojiboev <tbobur1995@gmail.com>
2023-11-18 15:00:04 +0900

    feat: develop nested routing for restaurant screen

---
+++ src/app/screens/RestaurantPage/allRestaurant.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function AllRestaurants() {
+  return <div>AllRestaurants</div>;
+}
---
+++ src/app/screens/RestaurantPage/chosenDish.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function ChosenDish() {
+  return <div>ChosenDish</div>;
+}
