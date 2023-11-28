65ef437fa0087c3592036f1c5f5c96f7ed565056
boburtojiboev <tbobur1995@gmail.com>
2023-11-28 14:36:56 +0900

    feat:develop member screen page publish

--- src/app/screens/MemberPage/index.tsx
+++ src/app/screens/MemberPage/index.tsx
@@ -1,6 +1,23 @@
 import React from "react";
 import { Container } from "@mui/material";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { VisitorOtherPage } from "./VisitOtherPage";
+import { VisitMyPage } from "./VisitMyPage";
+import "../../../css/my_page.css";
 
 export function MemberPage() {
-  return <Container>MemberPage</Container>;
+  let member = useRouteMatch();
+  console.log(member);
+  return (
+    <div className="restaurant_page">
+      <Switch>
+        <Route path={`${member.path}/other`}>
+          <VisitorOtherPage />
+        </Route>
+        <Route path={`${member.path}`}>
+          <VisitMyPage />
+        </Route>
+      </Switch>
+    </div>
+  );;
 }
